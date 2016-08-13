ec2e18648639b7ca0c330d1ccc4d1b9974f36f38
hgabka <hgabka@hgabka.eu>
2016-08-13 18:05:33 +0200

    javitas

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -45,7 +45,7 @@ class AppKernel extends Kernel
             new Kunstmaan\UserManagementBundle\KunstmaanUserManagementBundle(),
             new Kunstmaan\DashboardBundle\KunstmaanDashboardBundle(),
             new Webtown\DoctrineSchemaUpdateBundle\WebtownDoctrineSchemaUpdateBundle(),
-            new Hgabka\KunstmaanExtensionBundle\WebtownKunstmaanExtensionBundle(),
+            new Hgabka\KunstmaanExtensionBundle\HgabkaKunstmaanExtensionBundle(),
             new JMS\TranslationBundle\JMSTranslationBundle(),
             new Ivory\LuceneSearchBundle\IvoryLuceneSearchBundle(),
             new Hgabka\LuceneBundle\HgabkaLuceneBundle(),
